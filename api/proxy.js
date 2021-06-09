ae4d29e89dae9dbf06f2cefbb35448a739cac4d1
Funtience <769558413@qq.com>
2021-06-09 22:41:23 +0800

    update proxy +111

--- api/proxy.js
+++ api/proxy.js
@@ -7,7 +7,9 @@ module.exports = (req, res) => {
   // 处理代理目标地址
   if (req.url.startsWith('/api')) {
     target = 'http://106.75.18.179:1337/'
-  } 
+  } else if (req.url.startsWith('/uploads')) {
+    target = 'http://106.75.18.179:1337/'
+  }
 
   // 创建代理对象并转发请求
   createProxyMiddleware({
